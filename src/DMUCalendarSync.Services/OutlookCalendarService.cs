37fbaf795a70f7bd8101f556c2c5d2d9a6d71aa7
Arawn Davies <arawn.davies@gmail.com>
2021-11-25 18:17:40 +0000

    feat(outlookcalendar): add initial Outlook service client

---
+++ src/DMUCalendarSync.Services/OutlookCalendarService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace DMUCalendarSync.Services;
+
+public interface IOutlookCalendarService
+{
+}
+
+public class OutlookCalendarService : IOutlookCalendarService
+{
+    private readonly HttpClient _client;
+
+    public OutlookCalendarService(HttpClient client)
+    {
+        _client = client;
+    }
+}
