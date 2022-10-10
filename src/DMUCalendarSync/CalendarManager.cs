746636d4568cb03a6f1b21cd501245b0a0d48c1c
GOATS2K <jp@glattetre.com>
2022-10-11 00:39:42 +0100

    feat(calendarmanager): add 30 min reminder before lecture

--- src/DMUCalendarSync/CalendarManager.cs
+++ src/DMUCalendarSync/CalendarManager.cs
@@ -3,170 +3,170 @@ using System.Text;
 using System.Text.RegularExpressions;
 using DMUCalendarSync.Services;
 using DMUCalendarSync.Services.Models;
-using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3.Data;
 
-namespace DMUCalendarSync
+namespace DMUCalendarSync;
+
+public interface ICalendarManager
 {
-    public interface ICalendarManager
+    public Task SyncToGoogleCalendar();
+}
+
+internal class CalendarManager : ICalendarManager
+{
+    private readonly IMyDmuService _myDmuService;
+
+    public CalendarManager(IMyDmuService myDmuService)
     {
-        public Task SyncToGoogleCalendar();
+        _myDmuService = myDmuService;
     }
 
-    class CalendarManager : ICalendarManager
+    public async Task SyncToGoogleCalendar()
     {
-        private readonly IMyDmuService _myDmuService;
-
-        public CalendarManager(IMyDmuService myDmuService)
+        var dmuCalendar = await GetDmuCalendar();
+        if (dmuCalendar == null) return;
+        var gCalService = await GetGoogleCalendarService();
+        foreach (var calendarEvent in dmuCalendar.Events)
         {
-            _myDmuService = myDmuService;
-        }
+            var generatedEvent = CreateGCalEvent(calendarEvent);
+            var gCalEvents = await gCalService.GetEventsOnDay(calendarEvent.Start.Date);
+            var existingEvent = gCalEvents.Items.FirstOrDefault(e =>
+                e.Summary == generatedEvent.Summary
+                && e.Start.DateTime == generatedEvent.Start.DateTime
+                && e.End.DateTime == generatedEvent.End.DateTime);
 
-        public async Task SyncToGoogleCalendar()
-        {
-            var dmuCalendar = await GetDmuCalendar();
-            if (dmuCalendar == null)
+            if (existingEvent == null)
             {
-                return;
+                var syncedEvent = await gCalService.CreateCalendarEntry(generatedEvent);
+                Console.WriteLine($"Synced event: {generatedEvent.Summary}" +
+                                  $" at {calendarEvent.Start}" +
+                                  $" with GCal ID {syncedEvent.Id}");
             }
-            var gCalService = await GetGoogleCalendarService();
-            foreach (var calendarEvent in dmuCalendar.Events)
+            else
             {
-                var generatedEvent = CreateGCalEvent(calendarEvent);
-                var gCalEvents = await gCalService.GetEventsOnDay(calendarEvent.Start.Date);
-                var existingEvent = gCalEvents.Items.FirstOrDefault(e =>
-                    e.Summary == generatedEvent.Summary
-                    && e.Start.DateTime == generatedEvent.Start.DateTime
-                    && e.End.DateTime == generatedEvent.End.DateTime);
-
-                if (existingEvent == null)
+                if (ExistingEventHasChanged(existingEvent, calendarEvent))
                 {
-                    var syncedEvent = await gCalService.CreateCalendarEntry(generatedEvent);
-                    Console.WriteLine($"Synced event: {generatedEvent.Summary}" +
-                                      $" at {calendarEvent.Start}" +    
-                                      $" with GCal ID {syncedEvent.Id}");
+                    var updatedEvent = await gCalService.UpdateEvent(generatedEvent, existingEvent.Id);
+                    Console.WriteLine($"Updated event: {generatedEvent.Summary}" +
+                                      $" at {calendarEvent.Start}" +
+                                      $" with GCal ID {updatedEvent.Id}");
                 }
-                else
+            }
+        }
+    }
+
+    private bool ExistingEventHasChanged(Event existingEvent, CalendarEvent calendarEvent)
+    {
+        // check event hash
+        var bracketMatchingExpression = @"(?<=^\[).*?(?=\])";
+        var match = Regex.Match(existingEvent.Description,
+            bracketMatchingExpression,
+            RegexOptions.Multiline);
+
+        if (!match.Success) throw new ApplicationException("Unable to parse event hash from description");
+
+        if (match.Value != GenerateHashForEvent(calendarEvent)) return true;
+
+        return false;
+    }
+
+    private Event CreateGCalEvent(CalendarEvent calendarEvent)
+    {
+        var currentTime = DateTime.UtcNow;
+        var eventHash = GenerateHashForEvent(calendarEvent);
+        var parsedEventTitle = _myDmuService.ParseCalendarEventTitle(calendarEvent.Desc1!);
+
+        var gCalEvent = new Event
+        {
+            Start = new EventDateTime
+            {
+                DateTime = calendarEvent.Start
+            },
+            End = new EventDateTime
+            {
+                DateTime = calendarEvent.End
+            },
+            Summary = $"{parsedEventTitle.ModuleName} - {parsedEventTitle.ModuleId}",
+            Location = calendarEvent.LocAdd1,
+            Description = $"[{eventHash}] Synced at: {currentTime:s}",
+            Reminders = new Event.RemindersData
+            {
+                UseDefault = false,
+                Overrides = new List<EventReminder>
                 {
-                    if (ExistingEventHasChanged(existingEvent, calendarEvent))
+                    new()
                     {
-                        var updatedEvent = await gCalService.UpdateEvent(generatedEvent, existingEvent.Id);
-                        Console.WriteLine($"Updated event: {generatedEvent.Summary}" +
-                                          $" at {calendarEvent.Start}" +    
-                                          $" with GCal ID {updatedEvent.Id}");
+                        Method = "popup",
+                        Minutes = 30
                     }
                 }
             }
-        }
+        };
 
-        private bool ExistingEventHasChanged(Event existingEvent, CalendarEvent calendarEvent)
-        {
-            // check event hash
-            var bracketMatchingExpression = @"(?<=^\[).*?(?=\])";
-            var match = Regex.Match(existingEvent.Description,
-                bracketMatchingExpression,
-                RegexOptions.Multiline);
-            
-            if (!match.Success)
+        if (parsedEventTitle.Online)
+            gCalEvent.ConferenceData = new ConferenceData
             {
-                throw new ApplicationException("Unable to parse event hash from description");
-            }
-            
-            if (match.Value != GenerateHashForEvent(calendarEvent))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private Event CreateGCalEvent(CalendarEvent calendarEvent)
-        {
-            var currentTime = DateTime.UtcNow;
-            var eventHash = GenerateHashForEvent(calendarEvent);
-            var parsedEventTitle = _myDmuService.ParseCalendarEventTitle(calendarEvent.Desc1!);
-
-            var gCalEvent = new Event()
-            {
-                Start = new EventDateTime()
+                ConferenceId = eventHash,
+                ConferenceSolution = new ConferenceSolution
                 {
-                    DateTime = calendarEvent.Start
+                    Name = "Microsoft Teams",
+                    Key = new ConferenceSolutionKey
+                    {
+                        Type = "addOn"
+                    }
                 },
-                End = new EventDateTime()
+                EntryPoints = new List<EntryPoint>
                 {
-                    DateTime = calendarEvent.End
+                    new()
+                    {
+                        Label = "Teams Meeting URL",
+                        Uri = calendarEvent.MeetingURL,
+                        EntryPointType = "video"
+                    }
                 },
-                Summary = $"{parsedEventTitle.ModuleName} - {parsedEventTitle.ModuleId}",
-                Location = calendarEvent.LocAdd1,
-                Description = $"[{eventHash}] Synced at: {currentTime:s}"
+                Notes = calendarEvent.MeetingURLDesc
             };
 
-            if (parsedEventTitle.Online)
-            {
-                gCalEvent.ConferenceData = new ConferenceData()
-                {
-                    ConferenceId = eventHash,
-                    ConferenceSolution = new ConferenceSolution()
-                    {
-                        Name = "Microsoft Teams",
-                        Key = new ConferenceSolutionKey()
-                        {
-                            Type = "addOn"
-                        },
-                    },
-                    EntryPoints = new List<EntryPoint>()
-                    {
-                        new()
-                        {
-                            Label = "Teams Meeting URL",
-                            Uri = calendarEvent.MeetingURL,
-                            EntryPointType = "video"
-                        }
-                    },
-                    Notes = calendarEvent.MeetingURLDesc,
-                };
-            }
+        return gCalEvent;
+    }
 
-            return gCalEvent;
-        }
+    private async Task<CampusmCalendar?> GetDmuCalendar()
+    {
+        var username = Environment.GetEnvironmentVariable("DCS_DMU_USER");
+        var password = Environment.GetEnvironmentVariable("DCS_DMU_PASS");
 
-        private async Task<CampusmCalendar?> GetDmuCalendar()
+        if (username != null && password != null)
         {
-            var username = Environment.GetEnvironmentVariable("DCS_DMU_USER");
-            var password = Environment.GetEnvironmentVariable("DCS_DMU_PASS");
+            _myDmuService.SetCredentials(username, password);
+            var user = await _myDmuService.GetUser();
 
-            if (username != null && password != null)
-            {
-                _myDmuService.SetCredentials(username, password);
-                var user = await _myDmuService.GetUser();
-
-                Console.WriteLine($"Getting calendar data for: {user?.Email}");
-                return await _myDmuService.GetCalendar(DateTime.Today, DateTime.Today.AddDays(7));
-            }
-            return null;
+            Console.WriteLine($"Getting calendar data for: {user?.Email}");
+            return await _myDmuService.GetCalendar(DateTime.Today, DateTime.Today.AddDays(7));
         }
 
-        public async Task<Calendar> GetGoogleCalendar()
-        {
-            var gcal = await GetGoogleCalendarService();
-            return gcal.GetDCSCalendar();
-        }
+        return null;
+    }
 
-        private static async Task<GoogleCalendarService> GetGoogleCalendarService()
-        {
-            var calendarClient = await GoogleCalendarClient.ConfigureClient();
-            var gcal = new GoogleCalendarService(calendarClient);
-            return gcal;
-        }
+    public async Task<Calendar> GetGoogleCalendar()
+    {
+        var gcal = await GetGoogleCalendarService();
+        return gcal.GetDCSCalendar();
+    }
 
-        private string GenerateHashForEvent(CalendarEvent calendarEvent)
-        {
-            var uniqueCalendarString = calendarEvent.ToString();
-            
-            using var hasher = SHA256.Create();
-            var hashedBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(uniqueCalendarString));
+    private static async Task<GoogleCalendarService> GetGoogleCalendarService()
+    {
+        var calendarClient = await GoogleCalendarClient.ConfigureClient();
+        var gcal = new GoogleCalendarService(calendarClient);
+        return gcal;
+    }
 
-            return Convert.ToHexString(hashedBytes).Substring(0, 8);
-        }
+    private string GenerateHashForEvent(CalendarEvent calendarEvent)
+    {
+        var uniqueCalendarString = calendarEvent.ToString();
+
+        using var hasher = SHA256.Create();
+        var hashedBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(uniqueCalendarString));
+
+        return Convert.ToHexString(hashedBytes).Substring(0, 8);
     }
 }
